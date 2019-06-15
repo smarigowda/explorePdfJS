bd55a23a91138d0376260f4cc53a20543964392d
Santosh Marigowda <santosharakere@gmail.com>
2019-06-15 22:23:42 +0100

    restructured and refactored

---
+++ src/main.js
@@ -0,0 +1,130 @@
+console.log("...testing ok");
+
+const delay = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, 500);
+    })
+}
+
+
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window["pdfjs-dist/build/pdf"];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+  "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+
+let pdfDoc = null;
+let pageNum = 1;
+let pageRendering = false;
+let pageNumPending = null;
+let scale = 2;
+
+/**
+ * Get page info from document, resize canvas accordingly, and render page.
+ * @param num Page number.
+ */
+function renderPage(num) {
+  canvas = document.getElementById(`the-canvas-${num}`);
+  ctx = canvas.getContext("2d");
+  pageRendering = true;
+  // Using promise to fetch the page
+  pdfDoc.getPage(num).then(function(page) {
+    var viewport = page.getViewport({ scale: scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: ctx,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+
+    // Wait for rendering to finish
+    renderTask.promise.then(function() {
+      let ctx4 = document.getElementById(`the-canvas-${num}`).getContext("2d");
+      console.log(`${num}....ctx4`, ctx4);
+      ctx4.fillStyle = "white";
+      ctx4.fillRect(0, 0, 2000, 130);
+      pageRendering = false;
+      if (pageNumPending !== null) {
+        // New page rendering is pending
+        renderPage(pageNumPending);
+        pageNumPending = null;
+      }
+    });
+  });
+
+  // Update page counters
+  document.getElementById("page_num").textContent = num;
+}
+
+/**
+ * If another page rendering in progress, waits until the rendering is
+ * finised. Otherwise, executes rendering immediately.
+ */
+function queueRenderPage(num) {
+  if (pageRendering) {
+    pageNumPending = num;
+  } else {
+    renderPage(num);
+  }
+}
+
+/**
+ * Displays previous page.
+ */
+// function onPrevPage() {
+//   if (pageNum <= 1) {
+//     return;
+//   }
+//   pageNum--;
+//   queueRenderPage(pageNum);
+// }
+// document.getElementById("prev").addEventListener("click", onPrevPage);
+
+/**
+ * Displays next page.
+ */
+// function onNextPage() {
+//   if (pageNum >= pdfDoc.numPages) {
+//     return;
+//   }
+//   pageNum++;
+//   queueRenderPage(pageNum);
+// }
+// document.getElementById("next").addEventListener("click", onNextPage);
+
+
+var url = "./src/doc/chemistry.pdf";
+/**
+ * Asynchronously downloads PDF.
+ */
+pdfjsLib.getDocument(url).promise.then(async function(pdfDoc_) {
+  pdfDoc = pdfDoc_;
+//   document.getElementById("page_count").textContent = pdfDoc.numPages;
+
+  // Initial/first page rendering
+    // renderPage(pageNum);
+
+    await queueRenderPage(1);
+    await delay();
+    await queueRenderPage(2);
+    await delay();
+    await queueRenderPage(3);
+    await delay();
+
+//   renderPage(1);
+//   renderPage(2);
+//   renderPage(3);
+//   renderPage(4);
+//   renderPage(5);
+//   renderPage(6);
+//   renderPage(7);
+//   renderPage(8);
+//   renderPage(9);
+});

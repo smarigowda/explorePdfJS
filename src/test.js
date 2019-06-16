7cc33bea92a338193315d4bd20c1a809edf59087
Santosh Marigowda <santosharakere@gmail.com>
2019-06-16 09:27:29 +0100

    draft version of user interface

--- src/test.js
+++ src/test.js
@@ -1,6 +1,9 @@
 console.log("...PDFConverter");
 
-const path2pdf = "./src/doc/chemistry.pdf";
+let pdfFileName = null;
+let detailMaskTop = null;
+let headerMaskHeight = null;
+const saveNewPDF = false;
 let pdfDoc = null;
 const scale = 1.5;
 const newPdf = new jsPDF(); // loaded by script tag
@@ -11,6 +14,7 @@ function renderPage(num) {
     // addNewCanvasToHTMLPage()
     const element = document.createElement("canvas");
     element.setAttribute("id", `the-canvas-${num}`);
+    element.setAttribute("class", `the-canvas`);
     document.body.appendChild(element);
 
     // renderPageIntoCanvas()
@@ -30,10 +34,10 @@ function renderPage(num) {
         ctx.fillStyle = "white";
         // ctx4.fillStyle = "red";
         // maskHeaderInCanvas()
-        ctx.fillRect(0, 0, 2000, 130);
+        ctx.fillRect(0, 0, 2000, headerMaskHeight);
         if (num === 1) {
           // maskDetailInCanvas()
-          ctx.fillRect(0, 440, 2000, 2000);
+          ctx.fillRect(0, detailMaskTop, 2000, 2000);
         }
         // Add the masked canvas as a new page into the pdf document
         // addMaskedCanvasToNewPDF
@@ -51,16 +55,60 @@ function renderPage(num) {
   });
 }
 
-/**
- * Asynchronously downloads PDF.
- */
-// run()
-pdfjsLib.getDocument(path2pdf).promise.then(async doc => {
-  pdfDoc = doc;
-  console.log("total number of pages = ", pdfDoc.numPages);
-  for (let i = 1; i <= pdfDoc.numPages; i++) {
-    await renderPage(i);
-    i !== pdfDoc.numPages ? newPdf.addPage() : null;
-  }
-  newPdf.save("converted3.pdf");
-});
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("change", handleFile, false);
+
+function handleFile() {
+  const fileList = this.files; /* now you can work with the file list */
+  console.log(fileList);
+  console.log("file name = ", fileList[0].name);
+  pdfFileName = fileList[0].name;
+  renderPages();
+}
+console.log("inputElement...", inputElement);
+
+const renderAgainButton = document.getElementById("render-again");
+renderAgainButton.addEventListener("click", handleRenderAgain, false);
+
+function handleRenderAgain() {
+  document.querySelectorAll(".the-canvas").forEach(d => {
+    console.log(d);
+    d.remove();
+  });
+  document.getElementById("save-button").remove();
+  renderPages();
+}
+
+function renderPages() {
+  const path2pdf = `./src/doc/${pdfFileName}`;
+
+  const maskTop = document.getElementById("detail_mask_top");
+  console.dir(maskTop);
+  console.log("valueAsNumber", maskTop.valueAsNumber);
+  detailMaskTop = maskTop.valueAsNumber;
+
+  const headerMask = document.getElementById("header_mask_height");
+  console.log(headerMask);
+  headerMaskHeight = headerMask.valueAsNumber;
+
+  /**
+   * Asynchronously downloads PDF.
+   */
+  // run()
+  pdfjsLib.getDocument(path2pdf).promise.then(async doc => {
+    pdfDoc = doc;
+    console.log("total number of pages = ", pdfDoc.numPages);
+    for (let i = 1; i <= pdfDoc.numPages; i++) {
+      await renderPage(i);
+      i !== pdfDoc.numPages ? newPdf.addPage() : null;
+    }
+    const button = document.createElement("button");
+    button.setAttribute("id", `save-button`);
+    button.innerHTML = "Save PDF Now";
+    document.body.appendChild(button);
+    button.addEventListener("click", handleSave, false);
+    function handleSave() {
+      newPdf.save(`${pdfFileName}`);
+    }
+  });
+}
